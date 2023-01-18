82cd68cca1105279ad348a7091d8efd31bd3a34c
Dmitry Kuzmenko <dmitry.kuzmenko@dsr-corporation.com>
2023-01-18 12:19:05 +0300

    More updates on FFI

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,10 @@ pub mod derivation;
 pub mod error;
 pub mod event;
 pub mod event_parsing;
-pub mod ffi;
 pub mod keys;
 pub mod message;
 pub mod prefix;
 pub mod utils;
 
-pub use message::message::{Message, MessageList, FFIMessageListReturn, list_from_vec};
-pub use error::CesrError;
-pub use prefix::Prefix;
-pub use prefix::BasicPrefix;
-pub use prefix::SelfSigningPrefix;
-pub use prefix::self_addressing::SelfAddressingPrefix;
+pub use message::message::{Message, MessageList};
+pub use prefix::SelfAddressingPrefix;
--- uniffi/src/lib.rs
+++ uniffi/src/lib.rs
@@ -1,12 +1,19 @@
 pub use cesrox_core::error::CesrError;
-pub use cesrox_core::message::message::{list_from_vec, FFIMessageListReturn, Message, MessageList};
-pub use cesrox_core::prefix::self_addressing::SelfAddressingPrefix;
+pub use cesrox_core::error::CesrResult;
+pub use cesrox_core::message::message::{CesrMessage, CustomMessage, Message, MessageList};
 pub use cesrox_core::prefix::BasicPrefix;
 pub use cesrox_core::prefix::Prefix;
-pub use cesrox_core::prefix::SelfSigningPrefix;
 pub use cesrox_core::prefix::SelfAddressingPrefix;
+pub use cesrox_core::prefix::SelfSigningPrefix;
 
-pub fn list_from_vec(bytes: &Vec<u8>) -> CesrResult<FFIMessageListReturn> {
+use std::str::FromStr;
+
+pub struct FFIMessageListReturn {
+    pub rest: u64,
+    pub list: Vec<Message>,
+}
+
+pub fn parse_message_list(bytes: &Vec<u8>) -> CesrResult<FFIMessageListReturn> {
     let (rest, messages) = MessageList::from_stream(bytes).unwrap();
     return Ok(FFIMessageListReturn {
         rest: rest.len().try_into().unwrap(),
